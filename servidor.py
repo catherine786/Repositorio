83c396391d1bd3c8b5af217b9f2c151e5ced6322
catherine <83716701+catherine786@users.noreply.github.com>
2023-06-15 22:16:34 -0300

    Update servidor.py

--- servidor.py
+++ servidor.py
@@ -4,19 +4,24 @@ import threading
 def handle_client(client_socket, client_address):
     print(f"Conexión establecida desde {client_address[0]}:{client_address[1]}")
 
-    while True:
-        data = client_socket.recv(1024)
-        if not data:
-            break
-        print(f"Recibido desde {client_address[0]}:{client_address[1]}: {data.decode()}")
-        response = f"Mensaje recibido. Tu mensaje: {data.decode()}"
-        client_socket.sendall(response.encode())
+    try:
+        while True:
+            data = client_socket.recv(1024)
+            if not data:
+                break
+            print(f"Recibido desde {client_address[0]}:{client_address[1]}: {data.decode()}")
+            response = f"¡Mensaje recibido! Tú enviaste: {data.decode()}"
+            client_socket.sendall(response.encode())
+
+    except socket.error as e:
+        print(f"Error de socket: {e}")
+    finally:
         print(f"Conexión cerrada desde {client_address[0]}:{client_address[1]}")
         client_socket.close()
 
 def start_server(host, port):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((host, port))
+    server_socket.bind((host, port)) 
     server_socket.listen(5)
 
     print(f"El servidor esta escuchando en {host}:{port}")
@@ -27,7 +32,10 @@ def start_server(host, port):
         client_thread.start()
 
 
-HOST = 'localhost' 
-PORT = 12345 
+HOST = '127.0.0.1' 
+PORT = 3306 
 
 start_server(HOST, PORT)
+
+
+
